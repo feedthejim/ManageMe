cce01a075b0fa0c5349be80be540d57fb756182a
LAI Anh-Quan-Jimmy (SAFRAN ELECTRONICS FX586025 DEFENSE) <FX586025@sds.safran>
2016-05-24 16:47:10 +0200

    timeline wip

--- client/main.js
+++ client/main.js
@@ -3,5 +3,6 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/navbar.js';
-import '../imports/ui/main.js'
+import '../imports/ui/calendar.js';
+import '../imports/ui/main.js';
 
