1fa39a38ad367c96607d4e5acb78cdf76291ebf4
LAI Anh-Quan-Jimmy (SAFRAN ELECTRONICS FX586025 DEFENSE) <FX586025@sds.safran>
2016-05-27 13:46:11 +0200

    temporary timeline added + base account function

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../imports/startup/accounts-config.js';
 import '../imports/ui/navbar.js';
 import '../imports/ui/calendar.js';
 import '../imports/ui/main.js';
---
+++ imports/startup/accounts-config.js
@@ -0,0 +1,6 @@
+import { Accounts } from 'meteor/accounts-base';
+ 
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
+});
+accountsUIBootstrap3.setLanguage('fr');
