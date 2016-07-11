a6fc2e6e71e8329dbb23c28cfc237d07ca76d519
LAI Anh-Quan-Jimmy (SAFRAN ELECTRONICS FX586025 DEFENSE) <FX586025@sds.safran>
2016-07-11 14:15:29 +0200

    comments added

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,6 +1,24 @@
 import { Accounts } from 'meteor/accounts-base';
+
+
  
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
 accountsUIBootstrap3.setLanguage('fr');
+
+Comments.ui.config({
+   template: 'bootstrap' // or ionic, semantic-ui
+});
+
+Comments.ui.setContent({
+  title: 'Commentaires',
+  save: 'Sauvegarder',
+  reply: 'Répondre',
+  edit: 'Editer',
+  remove: 'Effacer',
+  'placeholder-textarea': 'Insérer votre commentaire',
+  'add-button-reply': 'Répondre à ce commentaire',
+  'add-button': 'Répondre',
+  'load-more': 'Afficher plus de commentaires'
+});
---
+++ setproxy.txt.bat
@@ -0,0 +1,3 @@
+SET HTTP_PROXY=http://FX586025:Jim11235813!+1@vipsds1.safran:8080
+SET HTTPS_PROXY=http://FX586025:Jim11235813!+1@vipsds1.safran:8080
+SET PATH=%PATH%;C:\Users\FX586025sav\AppData\Local\.meteor
